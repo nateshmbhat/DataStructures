f18c344557aea95bd2ee06a7473f3dce8a5fbc64
nateshmbhat <nateshmbhat1@gmail.com>
2017-12-05 12:48:24 +0530

    added insert and reverse list functions

--- Lists/Reversing a List/Non_recursive.c
+++ Lists/Reversing a List/Non_recursive.c
@@ -9,7 +9,7 @@ typedef struct node
 
 
 ///If the position is 0 insert at first  , else if pos = -1 insert at end , else insert at given position 
-node * insert_node( node * head , int data , int pos )
+void  insert_node( node * head , int data , int pos )
 {
     node * newnode = (node * )malloc(sizeof(node)) ; 
     newnode->data = data ;
@@ -30,12 +30,40 @@ node * insert_node( node * head , int data , int pos )
             //Temp now points to last node ;
 
             temp->next = newnode ;
+            newnode->next  = NULL ;
+        
+        default:
+            node * temp , *parent = head ;
+            temp = head->next ;
 
-            
+            int i = 0 ; 
+            for(i =1 ; temp && i<=pos  ; i++)
+                {
+                    parent = temp ; 
+                    temp = temp->next ; 
+                }
+
+            if(i!=pos)
+            {
+                printf("Invalid position ! ") ;
+                return ;
+            }
+            parent->next = newnode ;
+            newnode->next = temp ;
 
     }
+
+    head->data++ ;
 }
 
+
+void reverse_list(node * first)
+{
+    if(first == 0) return ; 
+
+    reverse_list(first->next ) ;
+    printf("%d ->" , first->info) ;
+}
 
 
 int main()
